237e602331c8381df051792b513c819b158edf22
Some Aditya Mandal <somaditya432@gmail.com>
2018-08-01 21:17:35 +0100

    BlogSummary CSV created

--- blog-summarizer.py
+++ blog-summarizer.py
@@ -4,22 +4,35 @@ from __future__ import division, print_function, unicode_literals
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
+
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer as SummarizerLR
+from sumy.summarizers.luhn import LuhnSummarizer as SummarizerLuhn
+from sumy.summarizers.text_rank import TextRankSummarizer as SummarizerTR
+from sumy.summarizers.edmundson import EdmundsonSummarizer as SummarizerEd
+
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
+from  bs4  import BeautifulSoup
+import urllib
 
 import csv
+import sys
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 LANGUAGE = "english"
 SENTENCES_COUNT = 5
 
 
+
 if __name__ == "__main__":
 
-	with open('blogslist.csv', 'rU') as csvfile:
+    with open('blogslist.csv', 'rU') as csvfile:
     	
-   		for row in csvfile:
+        summary = " "
+        for row in csvfile:
         		url = row
         		print(url)
     			parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
@@ -29,6 +42,13 @@ if __name__ == "__main__":
 
     			summarizer = Summarizer(stemmer)
     			summarizer.stop_words = get_stop_words(LANGUAGE)
+                
+                        for sentence in summarizer(parser.document, SENTENCES_COUNT):
+                            summary = summary + str(sentence).encode('utf-8')
+                            print(summary)
 
-    			for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        			print(sentence)
+                        with open('blogsummary.csv', 'a') as csvFile:
+                            row = [url,summary]
+                            writer = csv.writer(csvFile)
+                            writer.writerow(row)
+                            summary = " "
